9e0cb784fd42bd1d91ea906eb02463a3395103f4
meerathaakur <jayants2859@gmail.com>
2025-07-28 12:03:23 +0530

    convert hardcoded to efficient

--- config/multer.config.js
+++ config/multer.config.js
@@ -1,12 +1,13 @@
 const multer = require("multer");
 const path = require("path");
-const fs=require("fs")
+const fs=require("fs");
 
-if (!fs.existsSync("uploads")) fs.mkdirSync("uploads");
+const uploadDir=path.join(__dirname, "uploads")
+if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, "uploads/"),
-    filename: (req, file, cb) => {
+    destination: (_, __, cb) => cb(null, uploadDir),
+    filename: (_, file, cb) => {
         const uniqueSuffix = Date.now() + path.extname(file.originalname);
         cb(null, file.fieldname + "-" + uniqueSuffix);
     },
